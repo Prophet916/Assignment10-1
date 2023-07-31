8bd9aabe072b6d218f29b2425a9d7780fe85113a
Nicholas McDowell <Nicholas.mcdowell36@gmail.com>
2023-07-30 22:51:49 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment10_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Implement quick sort algorithm.
+             */
+
+            int[] arr = { 10, 7, 8, 9, 1, 5 };
+            Console.WriteLine("Original Array:");
+            PrintArray(arr);
+
+            QuickSortAlgorithm.QuickSort(arr);
+            Console.WriteLine("\nSorted Array:");
+            PrintArray(arr);
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ QuickSortAlgorithm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment10_1
+{
+    internal class QuickSortAlgorithm
+    {
+        public static void QuickSort(int[] arr)
+        {
+            if (arr == null || arr.Length <= 1)
+                return;
+
+            QuickSortHelper(arr, 0, arr.Length - 1);
+        }
+
+        private static void QuickSortHelper(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivotIndex = Partition(arr, left, right);
+
+                QuickSortHelper(arr, left, pivotIndex - 1);
+                QuickSortHelper(arr, pivotIndex + 1, right);
+            }
+        }
+
+        private static int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[right];
+            int i = left - 1;
+
+            for (int j = left; j < right; j++)
+            {
+                if (arr[j] <= pivot)
+                {
+                    i++;
+                    Swap(arr, i, j);
+                }
+            }
+
+            Swap(arr, i + 1, right);
+            return i + 1;
+        }
+
+        private static void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
